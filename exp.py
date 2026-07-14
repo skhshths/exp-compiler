517028ab4193069f52519f07a3dfe191faf29ab0
oliverbarnet <oliverbarnet441@gmail.com>
2026-07-14 14:22:01 -0400

    (7/13/26) RPI5 - variable stuff and started for loops

--- exp.py
+++ exp.py
@@ -9,7 +9,12 @@ class Compiler:
   
   def _redprint(self, message):
     print(f"\033[91m{message}\033[0m")
-  
+
+  def _loop(self, items):
+    target_var = items[1]
+    range = " ".join(items).split(" in ")[1].split("list(")[1].rstrip("):").split(", ")
+    print(f"{range=}")
+
   def _define_variable(self, var_name, value):
     """
     cases:
@@ -18,10 +23,29 @@ class Compiler:
      - x = 3
      - x = y
     """
+    split_values = value.split(" ")
     if any(item in "+-*/" for item in value):
-      if any(item in self.variables for item in value.split(" ")):
-        ... # HERE
+      if any(item in self.variables for item in split_values):
+        for index, item in enumerate(split_values):
+          if item in self.variables:
+            split_values[index] = self.variables[item]
+        try:
+          final = eval("".join(split_values))
+        except Exception as e:
+          self._redprint(e)
+          exit()
+        self.variables[var_name] = final
+        return 0
+      else:
+        try:
+          final = eval("".join(split_values))
+        except Exception as e:
+          self._redprint(e)
+          exit()
+        self.variables[var_name] = final
+        return 0
     self.variables[var_name] = value.strip("\"")
+    return 0
 
   def _comment(self):
     pass
@@ -51,7 +75,7 @@ class Compiler:
 
         current = "".join(letters)
         for instance in instances:
-          current = current.replace(instance, self.variables[instance.strip("{").strip("}")])
+          current = current.replace(instance, str(self.variables[instance.strip("{").strip("}")]))
         print(current)
       else:
         print(message.strip("\""))
@@ -71,8 +95,15 @@ class Compiler:
     
     # loop through all lines
     for line in data:
-      items = line.split(" ")
-      
+      if line.startswith(" "):
+        first_word = line.split()[0]
+        len_before_first_word = len(line.split(first_word)[0])
+        items = line.split()
+        for _ in range(len_before_first_word / 2):
+          items = ["|TAB|"] + items
+      else:
+        items = line.split()
+      print(f"{items=}")
       if line == "": continue
 
       if items[0] == ";":
@@ -81,6 +112,12 @@ class Compiler:
       elif items[0] == "print":
         self._print(" ".join(items).split("print ")[1])
       
+      elif items[0] == "for":
+        self._loop(items)
+
+      elif items[0] == " ":
+        pass
+
       else:
         self._define_variable(items[0], line.split(" = ")[1])
 
